cea30fe5803dd6882be1f7576951dda90262df9a
shaunak24 <shaunak241997@gmail.com>
2021-08-08 15:59:43 +0530

    Add navbar component and refactoring

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class UserItem extends Component {
+    render() {
+        return (
+            <div className='card text-center'>
+                UserItem
+            </div>
+        )
+    }
+}
+
+export default UserItem;
