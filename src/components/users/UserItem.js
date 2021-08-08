6cd4809327ab228019cd7a4dbd4739ad882c82b4
shaunak24 <shaunak241997@gmail.com>
2021-08-08 16:51:57 +0530

    Add user item component

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
 
 export class UserItem extends Component {
+    state = {
+        id: 1,
+        login: 'shaunak24',
+        avatar_url: 'https://avatars.githubusercontent.com/u/36746155?v=4',
+        html_url: 'https://github.com/shaunak24'
+    };
+
     render() {
+        const {login, avatar_url, html_url} = this.state;
+
         return (
             <div className='card text-center'>
-                UserItem
+                <img
+                    src={avatar_url}
+                    alt=''
+                    className='round-img'
+                    style={{width: '60px'}}
+                />
+                <h3>{login}</h3>
+                <div>
+                    <a href={html_url} className='btn btn-dark btn-sm my-1'>
+                        More
+                    </a>
+                </div>
             </div>
         )
     }
