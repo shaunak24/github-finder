1d798809d61707f9e41f1af826c8eb2a5495653f
shaunak24 <shaunak241997@gmail.com>
2021-08-08 19:34:31 +0530

    Add http request to render users

--- src/App.js
+++ src/App.js
@@ -4,12 +4,24 @@ import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 
 class App extends Component {
+  state = {
+    loading: false,
+    users: [],
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await fetch('https://api.github.com/users');
+    const data = await res.json();
+    this.setState({ loading: false, users: data });
+  }
+
   render() {
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,33 +2,10 @@ import React, { Component } from 'react';
 import UserItem from './UserItem';
 
 export class Users extends Component {
-  state = {
-    users: [
-      {
-        id: 1,
-        login: 'shaunak24',
-        avatar_url: 'https://avatars.githubusercontent.com/u/36746155?v=4',
-        html_url: 'https://github.com/shaunak24',
-      },
-      {
-        id: 2,
-        login: 'shaunak24',
-        avatar_url: 'https://avatars.githubusercontent.com/u/36746155?v=4',
-        html_url: 'https://github.com/shaunak24',
-      },
-      {
-        id: 4,
-        login: 'shaunak24',
-        avatar_url: 'https://avatars.githubusercontent.com/u/36746155?v=4',
-        html_url: 'https://github.com/shaunak24',
-      },
-    ],
-  };
-
   render() {
     return (
       <div style={userStyle}>
-        {this.state.users.map((user) => (
+        {this.props.users.map((user) => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
