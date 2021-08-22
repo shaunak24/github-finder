5e7dc80cbeb4c53760b09fd65ece3fa6c8e24f36
shaunak24 <shaunak241997@gmail.com>
2021-08-23 00:41:05 +0530

    Add Home and PageNotFound components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Alert from './components/layout/Alert';
 import Navbar from './components/layout/Navbar';
 import About from './components/pages/About';
-import Search from './components/users/Search';
 import User from './components/users/User';
-import Users from './components/users/Users';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
+import Home from './components/pages/Home';
+import PageNotFound from './components/pages/PageNotFound';
 
 export const App = () => {
   return (
@@ -20,18 +20,10 @@ export const App = () => {
             <div className='container'>
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={(props) => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                ></Route>
+                <Route exact path='/' component={Home}></Route>
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' component={User} />
+                <Route component={PageNotFound} />
               </Switch>
             </div>
           </div>
